9f427bdc4aa183b0563fc0913f41bae5b8178266
GustavoWillians <guswillians@outlook.com>
2025-10-03 16:27:40 -0300

    Removed older version of the project.

--- jornada_gamer/android/app/src/main/kotlin/gamer_journey/app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package gamer_journey.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- jornada_gamer/lib/state/mural_state.dart
+++
@@ -1,8 +0,0 @@
-// lib/state/mural_state.dart
-
-import '../models/milestone.dart';
-
-// Esta classe serve apenas para guardar a lista de marcos em memória.
-class MuralState {
-  static final List<Milestone> pinnedMilestones = [];
-}
